968a17c14c33d6acbaab3350dd830a52aa969741
LeeChungHyun <38039844+LeeChungHyun@users.noreply.github.com>
2021-01-26 15:44:06 +0900

    Add files via upload

---
+++ wordBook/GUI/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// WordBook.rc에서 사용되고 있습니다.
+//
+#define IDD_WORDBOOKFORM                101
+#define IDD_FINDINGFORM                 104
+#define IDD_MEMORYFORM                  106
+#define IDC_STATIC_WORD                 1002
+#define IDC_STATIC_SPELLING             1003
+#define IDC_STATIC_EXAMPLE              1004
+#define IDC_STATIC_WORDCLASS            1005
+#define IDC_STATIC_MEANING              1006
+#define IDC_EDIT_SPELLING               1007
+#define IDC_EDIT_MEANING                1008
+#define IDC_EDIT_EXAMPLE                1009
+#define IDC_EDIT_FINDMEANING            1009
+#define IDC_COMBO_WORDCLASS             1010
+#define IDC_BUTTON_RECORD               1011
+#define IDC_BUTTON_FIND                 1012
+#define IDC_BUTTON_ARRANGE              1013
+#define IDC_BUTTON_CORRECT              1014
+#define IDC_BUTTON_ERASE                1015
+#define IDC_LIST_WORDS                  1016
+#define IDC_STATIC_CONDITION            1018
+#define IDC_RADIO_FINDSPELLING          1019
+#define IDC_RADIO_FINDMEANING           1020
+#define IDC_EDIT_FINDSPELLING           1021
+#define IDC_LIST_TESTS                  1022
+#define IDC_LIST_ANSWERSHEETS           1022
+#define IDC_LIST_TESTS2                 1023
+#define IDC_STATIC_TEST                 1024
+#define IDC_STATIC_ANSWERSHEET          1025
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
